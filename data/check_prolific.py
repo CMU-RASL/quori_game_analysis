e4948a717c93e9a46ba6ed8f89e83f6733976449
roshk99 <roshk99@users.noreply.github.com>
2021-07-28 08:23:21 -0400

    Study 1 Data Processing
    - Added checking prolific codes match
    - Created all the graphs for study 1

---
+++ data/check_prolific.py
@@ -0,0 +1,62 @@
+import csv
+import pandas as pd
+import sqlite3
+
+prolific_file_name = 'data/study1_prolific.csv'
+db_file_name = 'data/study1.db'
+output_file_name = 'data/study1_prolific_approval.txt'
+
+#Read CSV
+ids = []
+statuses = []
+codes = []
+decisions = []
+with open(prolific_file_name, newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        ids.append(row['participant_id'])
+        statuses.append(row['status'])
+        codes.append(row['entered_code'])
+        if row['status'] == 'APPROVED':
+            decisions.append('ACCEPT')
+        else:
+            decisions.append('REJECT')
+        # if row['entered_code'] == 'i88ukKmKe':
+        #     print('here')
+prolific_tab = pd.DataFrame({'id': ids, 'status': statuses, 'code': codes, 'decision': decisions})
+
+#Read Database
+cnx = sqlite3.connect(db_file_name)
+user_tab = pd.read_sql_query("SELECT * from user", cnx)
+
+approved_ids = []
+
+#Iterate through Prolific Database
+for index in range(user_tab.shape[0]):
+    user_id = user_tab.at[index, 'username']
+    code = user_tab.at[index,'code']
+    
+    #Adjustments - Get this from adjustments.py!
+
+    #Get row in prolific tab
+    status = prolific_tab.loc[prolific_tab['id'] == user_id, 'status']
+    if status.shape[0] == 1:
+        
+        #Check if status matches
+        status = prolific_tab.loc[prolific_tab['id'] == user_id, 'status'].item()
+        if status == 'AWAITING REVIEW':
+            
+            #Check if code matches
+            prolific_code = prolific_tab.loc[prolific_tab['id'] == user_id, 'code'].item()
+
+            if code == prolific_code:
+                prolific_tab.loc[prolific_tab['id'] == user_id, 'decision'] = 'ACCEPT'
+                approved_ids.append(user_id)
+        if status == 'APPROVED':
+            approved_ids.append(user_id)
+
+#Write to text file
+with open(output_file_name, 'w') as txtfile:
+    for user_id in approved_ids:
+        txtfile.write(user_id)
+        txtfile.write('\n')
--- process_data_study1.py
+++ process_data_study1.py
@@ -11,10 +11,13 @@ import statsmodels.api as sm
 from statsmodels.formula.api import ols
 from scipy.stats import ttest_ind
 
+database_filename = 'data/study1.db'
+approved_ids_filename = 'data/study1_prolific_approval.txt'
+
 def read_tables():
 
     # Create your connection.
-    cnx = sqlite3.connect('data/pilot_study1.db')
+    cnx = sqlite3.connect(database_filename)
 
     condition_tab = pd.read_sql_query("SELECT * FROM condition", cnx)
 
@@ -74,26 +77,34 @@ def read_tables():
     return {'condition': condition_tab, 'user': user_tab, 'trial': trials_tab, 'demo': demos_tab, 'survey': survey_tab}
 
 def compile_data(tabs):
-    df = pd.DataFrame(columns=('difficulty', 'nonverbal', 'accuracy', 'user_id', 'condition_id', 'animacy', 'intelligence', 'user_learning'))
+    #Read approved_ids
+    approved_ids = []
+    with open(approved_ids_filename, 'r') as txtfile:
+        lines = txtfile.readlines()
+        for line in lines:
+            approved_ids.append(line[:-1])
+
+    df = pd.DataFrame(columns=('condition_id', 'user_id', 'accuracy', 'user_learning', 'animacy', 'intelligence', 'difficulty', 'engagement', 
+                                'answers', 'switches', 'switches_arr', 'elapsed_time', 'elapsed_time_arr', 'last_mistake'))
 
     for user_index in range(tabs['user'].shape[0]):
 
         #User Id
         user_id = tabs['user'].at[user_index, 'id'].item()
+        
+        username = tabs['user'].at[user_index, 'username']
 
-        #Condition Id
-        condition_id = tabs['user'].at[user_index, 'condition_id'].item()
+        #Only use approved ids
+        if username in approved_ids:
 
-        # Round Params
-        difficulty0 = tabs['condition'].loc[tabs['condition']['id'] == condition_id, 'difficulty0'].item()
-        difficulty1 = tabs['condition'].loc[tabs['condition']['id'] == condition_id, 'difficulty1'].item()
-        nonverbal0 = tabs['condition'].loc[tabs['condition']['id'] == condition_id, 'nonverbal0'].item()
-        nonverbal1 = tabs['condition'].loc[tabs['condition']['id'] == condition_id, 'nonverbal1'].item()
+            #Condition Id
+            condition_id = tabs['user'].at[user_index, 'condition_id'].item()
 
-        #Only use users that completed
-        test = tabs['survey'].loc[(tabs['survey']['round_num'] == 1) &
-                                             (tabs['survey']['user_id'] == user_id), ['animacy1', 'animacy2', 'animacy3']]
-        if test.shape[0] > 0:
+            # Round Params
+            difficulty0 = tabs['condition'].loc[tabs['condition']['id'] == condition_id, 'difficulty0'].item()
+            difficulty1 = tabs['condition'].loc[tabs['condition']['id'] == condition_id, 'difficulty1'].item()
+            nonverbal0 = tabs['condition'].loc[tabs['condition']['id'] == condition_id, 'nonverbal0'].item()
+            nonverbal1 = tabs['condition'].loc[tabs['condition']['id'] == condition_id, 'nonverbal1'].item()
 
             # For each round
             for round_num in [0, 1]:
@@ -119,21 +130,32 @@ def compile_data(tabs):
                 #Survey questions
                 animacy = tabs['survey'].loc[(tabs['survey']['round_num'] == round_num) &
                                                 (tabs['survey']['user_id'] == user_id), ['animacy1', 'animacy2', 'animacy3']]
-                animacy_avg = animacy.mean(axis=1)
+                if animacy.shape[0] == 2:
+                    animacy = animacy.to_numpy()
+                    animacy = animacy[0,:]
+                    animacy_avg = np.mean(animacy)
+                else:
+                    animacy_avg = animacy.mean(axis=1).item()
 
                 intelligence = tabs['survey'].loc[(tabs['survey']['round_num'] == round_num) &
                                                 (tabs['survey']['user_id'] == user_id), ['intelligence1', 'intelligence2']]
-                intelligence_avg = intelligence.mean(axis=1)
+                if intelligence.shape[0] == 2:
+                    intelligence = intelligence.to_numpy()
+                    intelligence = intelligence[0,:]
+                    intelligence_avg = np.mean(intelligence)
+                else:
+                    intelligence_avg = intelligence.mean(axis=1).item()
 
                 user_learning = tabs['survey'].loc[(tabs['survey']['round_num'] == round_num) &
-                                                (tabs['survey']['user_id'] == user_id), 'user_learning'].item()
+                                                (tabs['survey']['user_id'] == user_id), 'user_learning'].iloc[0]
+
 
                 difficulty = tabs['survey'].loc[(tabs['survey']['round_num'] == round_num) &
-                                                (tabs['survey']['user_id'] == user_id), 'difficulty'].item()
+                                                (tabs['survey']['user_id'] == user_id), 'difficulty'].iloc[0]
 
-                # engagement = tabs['survey'].loc[(tabs['survey']['round_num'] == round_num) &
-                #                                 (tabs['survey']['user_id'] == user_id), 'engagement'].item()
-                engagement = 2
+  
+                engagement = tabs['survey'].loc[(tabs['survey']['round_num'] == round_num) &
+                                                (tabs['survey']['user_id'] == user_id), 'engagement'].iloc[0]
                 
                 #Get time per trial
 
@@ -164,10 +186,11 @@ def compile_data(tabs):
                     else:
                         elapsed_time.append((current_time - previous_time).total_seconds())
                 elapsed_time_avg = np.mean(elapsed_time)
+
                 #Add to dataframe
                 data = {'condition_id': condition_id, 'user_id': user_id, 'accuracy': accuracy, 
                         'user_learning': user_learning, 'animacy': animacy_avg, 'intelligence': intelligence_avg,
-                        'difficulty': difficulty, 'engagement': engagement,
+                        'perceived_difficulty': difficulty, 'engagement': engagement,
                         'answers': answers['correct'].values.tolist(), 'switches': switches_avg, 'switches_arr': switches.values.tolist(),
                         'elapsed_time': elapsed_time_avg, 'elapsed_time_arr': elapsed_time,
                         'last_mistake': last_mistake}
@@ -181,6 +204,9 @@ def compile_data(tabs):
                 
                 df = df.append(data, ignore_index=True)
     
+    cols=['condition_id', 'user_id', 'user_learning', 'engagement', 'last_mistake']
+    df[cols] = df[cols].apply(pd.to_numeric, errors='coerce', axis=1)
+
     return(df)
 
 def plot_answers(data):
@@ -204,43 +230,43 @@ def plot_answers(data):
     difficult_optimal_std = np.std(difficult_all_prob_best, axis=0)
     
     #Top Graph - separated by difficulty - perfect
-    ax[0].plot(np.arange(8), easy_optimal_avg, label='Easy-Perfect-Best Card')
-    ax[1].plot(np.arange(8), difficult_optimal_avg, label='Difficult-Perfect-Best Card')
-    ax[0].plot(np.arange(8), easy_suboptimal_avg, label='Easy-Perfect-2nd Best Card')
-    ax[1].plot(np.arange(8), difficult_suboptimal_avg, label='Difficult-Perfect-2nd Best Card')
+    ax[0].errorbar(np.arange(8), easy_optimal_avg, yerr= easy_optimal_std,label='Easy-Perfect-Best Card')
+    ax[1].errorbar(np.arange(8), difficult_optimal_avg, yerr= difficult_optimal_std,label='Difficult-Perfect-Best Card')
+    ax[0].errorbar(np.arange(8), easy_suboptimal_avg, yerr= easy_suboptimal_std,label='Easy-Perfect-2nd Best Card')
+    ax[1].errorbar(np.arange(8), difficult_suboptimal_avg, yerr= difficult_suboptimal_std,label='Difficult-Perfect-2nd Best Card')
 
     #Top Graph - separated by difficulty - human
     easy_answers = np.vstack(data.loc[(data['difficulty'] == 'EASY'), 'answers'].to_numpy())
     easy_answers_avg = np.mean(easy_answers,axis=0)
     easy_answers_std = np.std(easy_answers,axis=0)
-    ax[0].plot(np.arange(8), easy_answers_avg, label='Easy-Human-Best Card')
+    ax[0].errorbar(np.arange(8), easy_answers_avg, yerr= easy_answers_std,label='Easy-Human-Best Card')
 
     difficulty_answers = np.vstack(data.loc[(data['difficulty'] == 'DIFFICULT'), 'answers'].to_numpy())
     difficulty_answers_avg = np.mean(difficulty_answers,axis=0)
     difficulty_answers_std = np.std(difficulty_answers,axis=0)
-    ax[1].plot(np.arange(8), difficulty_answers_avg, label='Difficult-Human-Best Card')
+    ax[1].errorbar(np.arange(8), difficulty_answers_avg, yerr= difficulty_answers_std,label='Difficult-Human-Best Card')
 
     #Bottom Graph - separated by movement - perfect
     optimal_answers = np.vstack((easy_optimal_avg, difficult_optimal_avg))
     optimal_avg = np.mean(optimal_answers, axis=0)
     optimal_std = np.std(optimal_answers, axis=0)
-    ax[2].plot(np.arange(8), optimal_avg, label='Perfect-Best Card')
+    ax[2].errorbar(np.arange(8), optimal_avg, yerr= optimal_std,label='Perfect-Best Card')
 
     suboptimal_answers = np.vstack((easy_suboptimal_avg, difficult_suboptimal_avg))
     suboptimal_avg = np.mean(suboptimal_answers, axis=0)
     suboptimal_std = np.std(suboptimal_answers, axis=0)
-    ax[2].plot(np.arange(8), suboptimal_avg, label='Perfect-2nd Best Card')
+    ax[2].errorbar(np.arange(8), suboptimal_avg, yerr= suboptimal_std,label='Perfect-2nd Best Card')
 
     #Bottom Graph - separated by movement - human
     neutral_answers = np.vstack(data.loc[(data['nonverbal'] == 'NEUTRAL'), 'answers'].to_numpy())
     neutral_answers_avg = np.mean(neutral_answers,axis=0)
     neutral_answers_std = np.std(neutral_answers,axis=0)
-    ax[2].plot(np.arange(8), neutral_answers_avg, label='Neutral-Human-Best Card')
+    ax[2].errorbar(np.arange(8), neutral_answers_avg, yerr= neutral_answers_std,label='Neutral-Human-Best Card')
 
     nonverbal_answers = np.vstack(data.loc[(data['nonverbal'] == 'NONVERBAL'), 'answers'].to_numpy())
     nonverbal_answers_avg = np.mean(nonverbal_answers,axis=0)
     nonverbal_answers_std = np.std(nonverbal_answers,axis=0)
-    ax[2].plot(np.arange(8), nonverbal_answers_avg, label='Nonverbal-Human-Best Card')
+    ax[2].errorbar(np.arange(8), nonverbal_answers_avg, yerr= nonverbal_answers_std,label='Nonverbal-Human-Best Card')
 
     ax[0].legend()
     ax[1].legend()
@@ -258,23 +284,23 @@ def plot_time_series(data, col_name, name):
     easy_answers = np.vstack(data.loc[(data['difficulty'] == 'EASY'), col_name].to_numpy())
     easy_answers_avg = np.mean(easy_answers,axis=0)
     easy_answers_std = np.std(easy_answers,axis=0)
-    ax[0].plot(np.arange(8), easy_answers_avg, label='Easy-Human-Best Card')
+    ax[0].errorbar(np.arange(8), easy_answers_avg, yerr=easy_answers_std, label='Easy-Human-Best Card')
 
     difficulty_answers = np.vstack(data.loc[(data['difficulty'] == 'DIFFICULT'), col_name].to_numpy())
     difficulty_answers_avg = np.mean(difficulty_answers,axis=0)
     difficulty_answers_std = np.std(difficulty_answers,axis=0)
-    ax[0].plot(np.arange(8), difficulty_answers_avg, label='Difficult-Human-Best Card')
+    ax[0].errorbar(np.arange(8), difficulty_answers_avg, yerr=difficulty_answers_std, label='Difficult-Human-Best Card')
 
     #Bottom Graph - separated by movement
     neutral_answers = np.vstack(data.loc[(data['nonverbal'] == 'NEUTRAL'), col_name].to_numpy())
     neutral_answers_avg = np.mean(neutral_answers,axis=0)
     neutral_answers_std = np.std(neutral_answers,axis=0)
-    ax[1].plot(np.arange(8), neutral_answers_avg, label='Neutral-Human-Best Card')
+    ax[1].errorbar(np.arange(8), neutral_answers_avg, yerr=neutral_answers_std, label='Neutral-Human-Best Card')
 
     nonverbal_answers = np.vstack(data.loc[(data['nonverbal'] == 'NONVERBAL'), col_name].to_numpy())
     nonverbal_answers_avg = np.mean(nonverbal_answers,axis=0)
     nonverbal_answers_std = np.std(nonverbal_answers,axis=0)
-    ax[1].plot(np.arange(8), nonverbal_answers_avg, label='Nonverbal-Human-Best Card')
+    ax[1].errorbar(np.arange(8), nonverbal_answers_avg, yerr=nonverbal_answers_std, label='Nonverbal-Human-Best Card')
 
     ax[0].legend()
     ax[1].legend()
@@ -284,6 +310,36 @@ def plot_time_series(data, col_name, name):
     plt.show()
 
 def two_way_anova(data, col_name):
+    fig, ax = plt.subplots()
+
+    easy = np.vstack(data.loc[(data['difficulty'] == 'EASY'), col_name].to_numpy())
+    easy_mean = np.mean(easy,axis=0)[0]
+    easy_std = np.std(easy,axis=0)[0]
+    
+    difficult = np.vstack(data.loc[(data['difficulty'] == 'DIFFICULT'), col_name].to_numpy())
+    difficult_mean = np.mean(difficult,axis=0)[0]
+    difficult_std = np.std(difficult,axis=0)[0]
+
+    neutral = np.vstack(data.loc[(data['nonverbal'] == 'NEUTRAL'), col_name].to_numpy())
+    neutral_mean = np.mean(neutral,axis=0)[0]
+    neutral_std = np.std(neutral,axis=0)[0]
+
+    nonverbal = np.vstack(data.loc[(data['nonverbal'] == 'NONVERBAL'), col_name].to_numpy())
+    nonverbal_mean = np.mean(nonverbal,axis=0)[0]
+    nonverbal_std = np.std(nonverbal,axis=0)[0]
+    
+    labels = ['Easy', 'Difficult', 'Neutral', 'Nonverbal']
+    x_pos = np.arange(4)
+    means = [easy_mean, difficult_mean, neutral_mean, nonverbal_mean]
+    stds = [easy_std, difficult_std, neutral_std, nonverbal_std]
+
+    ax.bar(x_pos, means, yerr=stds, align='center', alpha=0.5, ecolor='black', capsize=10)
+    ax.set_ylabel(col_name)
+    ax.set_xticks(x_pos)
+    ax.set_xticklabels(labels)
+    ax.yaxis.grid(True)
+    # plt.show()
+
     model = ols(col_name + ' ~ C(difficulty) + C(nonverbal) + C(difficulty):C(nonverbal)', data=data).fit()
     anova_table = sm.stats.anova_lm(model, type=2)
     print('---')
@@ -322,9 +378,9 @@ def compare_dists_answers(data):
                 t_values.append(ttest_ind(p, q, equal_var=False)[0])
                 p_values.append(ttest_ind(p, q, equal_var=False)[1])
             df_t.at[row_name, col_name] = t_values
-            df_p.at[row_name, col_name] = p_values
+            df_p.at[row_name, col_name] = np.where(np.array(p_values) < 0.05)
     
-    print(np.round(df_p.at['Easy-Perfect-Best Card', 'Easy-Human-Best Card'], decimals=3))
+    print(df_p)
 
 def compare_dists_other(data, col_name):
     easy_answers = np.vstack(data.loc[(data['difficulty'] == 'EASY'), col_name].to_numpy())
@@ -348,7 +404,7 @@ def compare_dists_other(data, col_name):
                 t_values.append(ttest_ind(p, q, equal_var=False)[0])
                 p_values.append(ttest_ind(p, q, equal_var=False)[1])
             df_t.at[row_name, col_name] = t_values
-            df_p.at[row_name, col_name] = p_values
+            df_p.at[row_name, col_name] = np.where(np.array(p_values) < 0.05)
     
     print(df_p)
 
@@ -371,13 +427,28 @@ def expected_accuracy():
 if __name__ == '__main__':
     tabs = read_tables()
     data = compile_data(tabs)
-    # print(tabs['survey'])
-    # print(data[['difficulty', 'answers', 'user_id', 'last_mistake']])
-    # print(tabs['user'])
+
+    '''
+    ['condition_id', 'user_id', 'accuracy', 'user_learning', 'animacy',
+        'intelligence', 'difficulty', 'engagement', 'answers', 'switches',
+        'switches_arr', 'elapsed_time', 'elapsed_time_arr', 'last_mistake',
+        'nonverbal', 'perceived_difficulty']
+    '''
+
     # plot_answers(data)
     # plot_time_series(data, 'elapsed_time_arr', 'Elapsed Time (sec)')
     # plot_time_series(data, 'switches_arr', 'Number of Switches')
+
     # two_way_anova(data, 'last_mistake')
+    # two_way_anova(data, 'accuracy')
+    # two_way_anova(data, 'user_learning')
+    # two_way_anova(data, 'perceived_difficulty')
+    # two_way_anova(data, 'engagement')
+    # two_way_anova(data, 'animacy')
+    # two_way_anova(data, 'intelligence')
+    # two_way_anova(data, 'elapsed_time')
+    # two_way_anova(data, 'switches')
+
     # compare_dists_answers(data)
     # compare_dists_other(data, 'elapsed_time_arr')
     # compare_dists_other(data, 'switches_arr')
