769fd0f8ff99bdbc9e408d8cfb1956066c12cdd6
roshk99 <roshk99@users.noreply.github.com>
2021-10-14 15:48:52 -0500

    Updates for study 2 - create time series

---
+++ data/params_study2.py
@@ -0,0 +1,18 @@
+DEBUG_MODE = False
+
+# CONDITIONS = [(('EASY', 'NONVERBAL'), ('DIFFICULT', 'NONVERBAL')), (('EASY', 'NEUTRAL'), ('DIFFICULT', 'NEUTRAL')), (('DIFFICULT', 'NONVERBAL'), ('EASY', 'NONVERBAL')), (('DIFFICULT', 'NEUTRAL'), ('EASY', 'NEUTRAL')), 
+# (('EASY', 'NEUTRAL'), ('DIFFICULT', 'NONVERBAL')), (('EASY', 'NONVERBAL'), ('DIFFICULT', 'NEUTRAL')), (('DIFFICULT', 'NEUTRAL'), ('EASY', 'NONVERBAL')), (('DIFFICULT', 'NONVERBAL'), ('EASY', 'NEUTRAL')),
+# ]
+CONDITIONS = [(('EASY', 'NONVERBAL'), ('DIFFICULT', 'NONVERBAL')), (('DIFFICULT', 'NONVERBAL'), ('EASY', 'NONVERBAL'))]
+
+RULE_PROPS = {'EASY': {'rule': 'diamonds on left, all others on right', 'demo_cards': [54, 60], 'cards': [48, 47, 10, 58, 14, 18, 57, 32], 'demo_answers': [0, 1], 'answers': [1, 0, 0, 1, 1, 0, 1, 1]},
+            'DIFFICULT': {'rule': 'green-one, red/purple on left, green two/three on right', 'demo_cards': [61, 34], 'cards': [33, 32, 42, 17, 68, 29, 26, 45],'demo_answers': [0, 1], 'answers': [0, 1, 0, 0, 0, 1, 0, 0]}}
+
+FEEDBACK = {
+    'NEUTRAL': {'CORRECT-RIGHT': ['neutral_correct_right_1', 'neutral_correct_right_2'], 'INCORRECT-RIGHT': ['neutral_incorrect_right_1', 'neutral_incorrect_right_2', ],'CORRECT-LEFT': ['neutral_correct_left_1', 'neutral_correct_left_2'], 'INCORRECT-LEFT': ['neutral_incorrect_left_1', 'neutral_incorrect_left_2']},
+    'NONVERBAL': {'CORRECT-RIGHT': ['nonverbal_correct_right_1', 'nonverbal_correct_right_2', 'nonverbal_correct_right_3', 'nonverbal_correct_right_4'], 'INCORRECT-RIGHT': ['nonverbal_incorrect_right_1', 'nonverbal_incorrect_right_2', 'nonverbal_incorrect_right_3', 'nonverbal_incorrect_right_4'],'CORRECT-LEFT': ['nonverbal_correct_left_1', 'nonverbal_correct_left_2', 'nonverbal_correct_left_3', 'nonverbal_correct_left_4'], 'INCORRECT-LEFT': ['nonverbal_incorrect_left_1', 'nonverbal_incorrect_left_2', 'nonverbal_incorrect_left_3', 'nonverbal_incorrect_left_4']}
+}
+
+NEUTRAL = ['neutral_long_1', 'neutral_long_2', 'neutral_long_3']
+
+VIDEO_LIST = ['neutral_long_1', 'neutral_long_2', 'neutral_long_3', 'neutral_correct_right_1', 'neutral_correct_right_2', 'neutral_incorrect_right_1', 'neutral_incorrect_right_2', 'neutral_correct_left_1', 'neutral_correct_left_2', 'neutral_incorrect_left_1', 'neutral_incorrect_left_2', 'nonverbal_correct_right_1', 'nonverbal_correct_right_2', 'nonverbal_correct_right_3', 'nonverbal_correct_right_4', 'nonverbal_incorrect_right_1', 'nonverbal_incorrect_right_2', 'nonverbal_incorrect_right_3', 'nonverbal_incorrect_right_4', 'nonverbal_correct_left_1', 'nonverbal_correct_left_2', 'nonverbal_correct_left_3', 'nonverbal_correct_left_4', 'nonverbal_incorrect_left_1', 'nonverbal_incorrect_left_2', 'nonverbal_incorrect_left_3', 'nonverbal_incorrect_left_4']
--- process_data_study2.py
+++ process_data_study2.py
@@ -1,37 +1,43 @@
 import sqlite3
 import pandas as pd
-from data.params import *
+from data.params_study2 import *
+import os
 from datetime import datetime
 import numpy as np
-from data.utils import *
-from os import path
 
-def read_tables():
+database_filename = 'data/study2_test.db'
+features_filename = 'data/study2_test_users/'
+easy_answers = [1, 0, 0, 1, 1, 0, 1, 1]
+difficult_answers = [0, 1, 0, 0, 0, 1, 0, 0]
 
+def read_tables():   
     # Create your connection.
-    cnx = sqlite3.connect('data/app.db')
+    cnx1 = sqlite3.connect(database_filename)
 
-    condition_tab = pd.read_sql_query("SELECT * FROM condition", cnx)
+    #Condition
+    condition_tab = pd.read_sql_query("SELECT * FROM condition", cnx1)
 
     #Add new columns, for each round
     condition_tab['difficulty0'] = ['', '']
     condition_tab['difficulty1'] = ['', '']
+    condition_tab['nonverbal0'] = ['', '']
+    condition_tab['nonverbal1'] = ['', '']
     for index in range(condition_tab.shape[0]):
         condition_tab.at[index, 'difficulty'] = (CONDITIONS[index][0][0], CONDITIONS[index][1][0])
         condition_tab.at[index, 'nonverbal'] = (CONDITIONS[index][0][1], CONDITIONS[index][1][1])
         condition_tab.at[index, 'difficulty0'] = CONDITIONS[index][0][0]
         condition_tab.at[index, 'difficulty1'] = CONDITIONS[index][1][0]
+        condition_tab.at[index, 'nonverbal0'] = CONDITIONS[index][0][1]
+        condition_tab.at[index, 'nonverbal1'] = CONDITIONS[index][1][1]
+    
+    #User
+    user_tab = pd.read_sql_query("SELECT * from user", cnx1)
 
-    # print(condition_tab)
-
-    user_tab = pd.read_sql_query("SELECT * from user", cnx)
-    # print(user_tab)
-
-    trials_tab = pd.read_sql_query("SELECT * from trial", cnx)
+    trials_tab = pd.read_sql_query("SELECT * from trial", cnx1)
     for index in range(trials_tab.shape[0]):
         #Get User id
         user_id = trials_tab.at[index, 'user_id'].item()
-        
+
         #Get condition id
         condition_id = user_tab.loc[user_tab['id'] == user_id, 'condition_id'].item()
         
@@ -54,186 +60,111 @@ def read_tables():
         #Get rule
         trials_tab.at[index, 'rule_set'] = RULE_PROPS[difficulty]['rule']
 
-    # print(trials_tab)
+    demos_tab = pd.read_sql_query("SELECT * from demo", cnx1)
 
-    demos_tab = pd.read_sql_query("SELECT * from demo", cnx)
-    # print(demos_tab)
+    survey_tab = pd.read_sql_query("SELECT * from survey", cnx1)
+   
+    return {'condition': condition_tab, 'user': user_tab, 'trial': trials_tab, 'demo': demos_tab, 'survey': survey_tab}
 
-    return {'condition': condition_tab, 'user': user_tab, 'trial': trials_tab, 'demo': demos_tab}
+def read_features():
+    features = []
+    for user_folder in os.listdir(features_filename):
+        features_folder = '{}{}/features'.format(features_filename, user_folder)
+        user_id = features_folder.split('/')[-2]
+        for feature_file in os.listdir(features_folder):
+            cur_feat = pd.read_csv('{}/{}'.format(features_folder, feature_file))
+            if (cur_feat.shape[0] == 1):
+                if not pd.isna(cur_feat['FaceRectX'].item()):
+                    cur_feat['user'] = user_id
 
-def card_order_values(card_order, true_hyp, all_hyp, cards):
-    card_num = len(card_order)
-    num_bins = np.array([0, 0])
-    num_hypotheses_arr = np.zeros((card_num))
-    equiv_cards_arr = np.zeros((card_num))
-    prob_arr = np.zeros((card_num))
-    hyp_valid = np.ones((len(all_hyp))).astype('int')
+                    timestamp = cur_feat['input'].item().split('/')[-1][5:-4]
+                    timestamp = datetime.strptime(timestamp, "%Y-%m-%d_%H-%M-%S")
+                    cur_feat['timestamp'] = timestamp
 
-    for ind in range(card_num):
-        
-        #Number of hypotheses remaining
-        num_hypotheses_arr[ind] = np.sum(hyp_valid)
+                    features.append(cur_feat)
+                    
+    features_tab = pd.concat(features)
+    return features_tab
 
-        #Find number of hypotheses eliminated by each card
-        num_hyp_removed = np.zeros((81))
-        hyp_removed_ind = []
-        for test_card in range(81):
-            if test_card in card_order[:ind]:
-                num_hyp_removed[test_card] = -10
-                hyp_removed_ind.append([])
-            else:
-                tmp1, tmp2 = calc_hyp_removed(all_hyp, hyp_valid, true_hyp, cards[test_card,:,:])
-                num_hyp_removed[test_card] = tmp1
-                hyp_removed_ind.append(tmp2)
+def compile_data(tabs, features):
 
-        #Cards eliminating the maximum number of hypotheses
-        max_val = np.max(num_hyp_removed)
-        max_inds = np.where(num_hyp_removed == max_val)[0]
-        equiv_cards_arr[ind] = len(max_inds)
+    data = []
 
-        #Chosen card
-        current_card_ind = card_order[ind]
-        current_card = cards[current_card_ind, :, :]
-        current_bin = sort_card(true_hyp, current_card)
-        
-        #Find probability that we would have sorted this card correctly
-        num_corr = 0
-        for hyp_ind in np.arange(len(hyp_valid)):
-            if hyp_valid[hyp_ind]:
-                if sort_card(all_hyp[hyp_ind], current_card) == current_bin:
-                    num_corr += 1
-        prob_arr[ind] = num_corr/float(np.sum(hyp_valid))
+    for user_id in tabs['user']['id'].tolist():
 
-        #Add card and remove hypotheses
-        hyp_valid[hyp_removed_ind[current_card_ind]] = 0
-        num_bins[current_bin] += 1
+        #Check if final survey is completed
+        final_survey = tabs['survey'].loc[(tabs['survey']['user_id'] == user_id) & (tabs['survey']['round_num'] == 1)]
+        if final_survey.shape[0] == 1:
 
-    return num_hypotheses_arr, equiv_cards_arr, prob_arr   
+            #Get condition
+            condition_id = tabs['user'].loc[(tabs['user']['id'] == user_id), 'condition_id'].item()
 
-def compute_features(tabs, rule_params):
-    features = np.zeros((tabs['trial'].shape[0], 8))
+            # Round Params
+            difficulty0 = tabs['condition'].loc[tabs['condition']['id'] == condition_id, 'difficulty0'].item()
+            difficulty1 = tabs['condition'].loc[tabs['condition']['id'] == condition_id, 'difficulty1'].item()
+            nonverbal0 = tabs['condition'].loc[tabs['condition']['id'] == condition_id, 'nonverbal0'].item()
+            nonverbal1 = tabs['condition'].loc[tabs['condition']['id'] == condition_id, 'nonverbal1'].item()
 
-    for index in range(tabs['trial'].shape[0]):
-        #Get User id
-        user_id = tabs['trial'].at[index, 'user_id'].item()
-        
-        #Get condition id
-        condition_id = tabs['user'].loc[tabs['user']['id'] == user_id, 'condition_id'].item()
+            #Get start and end times
+            start_time = []
+            end_time = []
+            answers_arr = []
+            confidence_arr = []
+            rounds = 2*[1, 2, 3, 4, 5, 6, 7, 8]
+            user_arr = 16*[user_id]
+            features_arr = []
+            for round_num in [0, 1]:
+                
+                #Accuracy
+                answers = tabs['trial'].loc[(tabs['trial']['round_num'] == round_num) &
+                                                (tabs['trial']['user_id'] == user_id), ['correct_bin', 'chosen_bin']]
+                if (round_num == 0 and difficulty0 == 'EASY') or (round_num == 1 and difficulty1 == 'EASY'):
+                    answers['correct_bin'] = easy_answers
+                else:
+                    answers['correct_bin'] = difficult_answers
+                
+                answers_arr.extend(np.where(answers['correct_bin'] == answers['chosen_bin'], 1, 0))
 
-        #Get round
-        round_num = tabs['trial'].at[index, 'round_num'].item()
+                #Get time of 2nd demo
+                demo_time = tabs['demo'].loc[(tabs['demo']['user_id'] == user_id) & 
+                                        (tabs['demo']['round_num'] == round_num) &
+                                        (tabs['demo']['demo_num'] == 2), 'timestamp'].iloc[0]
+                demo_time = datetime.strptime(demo_time, '%Y-%m-%d %H:%M:%S.%f')
+                
+                start_time.append(demo_time)
+                #All trials
+                for trial_num in range(1, 9):
+                    trial_time = tabs['trial'].loc[(tabs['trial']['user_id'] == user_id) &
+                                            (tabs['trial']['round_num'] == round_num) &
+                                            (tabs['trial']['trial_num'] == trial_num), 'timestamp'].iloc[0]
+                    trial_time = datetime.strptime(trial_time, '%Y-%m-%d %H:%M:%S.%f')
+                    end_time.append(trial_time)
+                    if trial_num < 8:
+                        start_time.append(trial_time)
+                    
 
-        #Number of card presented
-        trial_num = tabs['trial'].at[index, 'trial_num'].item()
+                    confidence = tabs['trial'].loc[(tabs['trial']['user_id'] == user_id) &
+                                            (tabs['trial']['round_num'] == round_num) &
+                                            (tabs['trial']['trial_num'] == trial_num), 'confidence'].iloc[0]
+                    confidence_arr.append(confidence)
+                        
+            for start, end in zip(start_time, end_time):
+                features_arr.append(features.loc[(features['user'] == str(user_id)) &
+                                    (features['timestamp'] >= start) & 
+                                    (features['timestamp'] <= end)])
 
-        #Get difficulty
-        if round_num == 0:
-            difficulty = tabs['condition'].loc[tabs['condition']['id'] == condition_id, 'difficulty0'].item()
-        else:
-            difficulty = tabs['condition'].loc[tabs['condition']['id'] == condition_id, 'difficulty1'].item()
 
-        #Time of first demo
-        first_demo_time = tabs['demo'].loc[(tabs['demo']['user_id'] == user_id) & 
-                                            (tabs['demo']['demo_num'] == 1) & 
-                                            (tabs['demo']['round_num'] == round_num), 'timestamp'].item()
-        first_demo_time = datetime.strptime(first_demo_time, '%Y-%m-%d %H:%M:%S.%f')
+            df = pd.DataFrame({'start_time': start_time, 'end_time': end_time, 'answer': answers_arr, 'confidence': confidence, 'round': rounds, 'user': user_arr, 'features': features_arr})
+            data.append(df)
+    
+    data = pd.concat(data)
+    return(data)
 
-        #Time of current trial start
-        trial_time = tabs['trial'].at[index, 'timestamp']
-        trial_time = datetime.strptime(trial_time, '%Y-%m-%d %H:%M:%S.%f')
-
-        #Get all previous accuracies
-        prev_acc_arr = np.zeros((trial_num-1))
-        for prev_trial_num in range(1, trial_num):
-            prev_correct_bin = tabs['trial'].loc[(tabs['trial']['user_id'] == user_id) & 
-                                            (tabs['trial']['trial_num'] == trial_num - 1) & 
-                                            (tabs['trial']['round_num'] == round_num), 'correct_bin'].item()
-            prev_chosen_bin = tabs['trial'].loc[(tabs['trial']['user_id'] == user_id) & 
-                                            (tabs['trial']['trial_num'] == trial_num - 1) & 
-                                            (tabs['trial']['round_num'] == round_num), 'chosen_bin'].item()
-            if prev_correct_bin == prev_chosen_bin:
-                prev_acc_arr[prev_trial_num-1] = 1
-
-        #FEATURES
-
-        #Rule Difficulty (Easy = 0, Difficult = 1)
-        if difficulty == 'EASY':
-            task_difficulty = 0
-        else:
-            task_difficulty = 1
-        
-        #Number of card presented
-        #trial_num
-        
-        #Time Elapsed in seconds
-        time_elapsed = (trial_time - first_demo_time).total_seconds()
-
-        #Hyp Number before card
-        hyp_before_card = rule_params[difficulty]['num_hypotheses'][trial_num-1]
-
-        #Probability of sorting correctly
-        prob_correct_sort = rule_params[difficulty]['prob'][trial_num-1]
-
-        #Accuracy of previous card
-        if len(prev_acc_arr) == 0:
-            prev_acc = 0.
-        else:
-            prev_acc = prev_acc_arr[-1]
-        
-        #Accuracy of previous 4 cards
-        if len(prev_acc_arr) == 0:
-            prev_4_acc = 0.
-        elif len(prev_acc_arr) < 4:
-            prev_4_acc = np.mean(prev_acc_arr)
-        else:
-            prev_4_acc = np.mean(prev_acc_arr[-4:])
-        
-        #Accuracy of all cards
-        if len(prev_acc_arr) == 0:
-            prev_all_acc = 0.
-        else:
-            prev_all_acc = np.mean(prev_acc_arr)
-        
-        features[index, :] = [task_difficulty, trial_num, time_elapsed, hyp_before_card, prob_correct_sort, prev_acc, prev_4_acc, prev_all_acc]
-
-    return features
 
 if __name__ == '__main__':
     tabs = read_tables()
-    
-    blank_hyp = np.zeros((2, 10, 4, 3))
-    all_hyp = create_all_hypotheses()
-    all_cards = create_all_cards()
+    features = read_features()
 
-    easy_demos = RULE_PROPS['EASY']['demo_cards']
-    easy_cards = RULE_PROPS['EASY']['cards']
-    easy_cards.extend(easy_demos)
-    easy_rule = np.copy(blank_hyp)
-    easy_rule[0, 0, 2, 0] = 1
-    easy_rule[1, 0, 2, 1] = 1
-    easy_rule[1, 1, 2, 2] = 1
+    data = compile_data(tabs, features)
 
-    if not path.exists('data/easy.npz'):
-        easy_num_hypotheses, easy_equiv_cards, easy_prob = card_order_values(easy_cards, easy_rule, all_hyp, all_cards)
-        np.savez('data/easy.npz', num_hypotheses=easy_num_hypotheses[len(easy_demos):], 
-                                    equiv_cards=easy_equiv_cards[len(easy_demos):], 
-                                    prob=easy_prob[len(easy_demos):])
-    easy_params = np.load('data/easy.npz')
-
-    difficult_demos = RULE_PROPS['DIFFICULT']['demo_cards']
-    difficult_cards = RULE_PROPS['DIFFICULT']['cards']
-    difficult_cards.extend(difficult_demos)
-    difficult_rule = np.copy(blank_hyp)
-    difficult_rule[0, 0, 2, 0] = 1
-    difficult_rule[1, 0, 2, 1] = 1
-    difficult_rule[1, 1, 2, 2] = 1
-
-    if not path.exists('data/difficult.npz'):
-        difficult_num_hypotheses, difficult_equiv_cards, difficult_prob = card_order_values(difficult_cards, difficult_rule, all_hyp, all_cards)
-        np.savez('data/difficult.npz', num_hypotheses=difficult_num_hypotheses[len(difficult_demos):], 
-                                    equiv_cards=difficult_equiv_cards[len(difficult_demos):], 
-                                    prob=difficult_prob[len(difficult_demos):])
-    difficult_params = np.load('data/difficult.npz')
-
-    features = compute_features(tabs, {'EASY': easy_params, 'DIFFICULT': difficult_params})
     
